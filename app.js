ddab5dd0487047b734930a61c8a9937ca61134cd
Gabrielasramalho <99361220+Gabrielasramalho@users.noreply.github.com>
2022-07-24 17:59:38 -0300

    trab_final

---
+++ app.js
@@ -0,0 +1,73 @@
+function alterarPopup1() {
+  //pega o elemento da classe popup1 e cria uma classe ativa
+  document.getElementById("popup1").classList.toggle("ativa1");
+}
+
+function alterarPopup2() {
+  document.getElementById("popup2").classList.toggle("ativa2");
+}
+function alterarPopup3() {
+  document.getElementById("popup3").classList.toggle("ativa3");
+}
+
+function alterarPopup4() {
+  document.getElementById("popup4").classList.toggle("ativa4");
+}
+function alterarPopup5() {
+  document.getElementById("popup5").classList.toggle("ativa5");
+}
+function alterarPopup6() {
+  document.getElementById("popup6").classList.toggle("ativa6");
+}
+function alterarPopup7() {
+  document.getElementById("popup7").classList.toggle("ativa7");
+}
+function alterarPopup8() {
+  document.getElementById("popup8").classList.toggle("ativa8");
+}
+function alterarPopup9() {
+  document.getElementById("popup9").classList.toggle("ativa9");
+}
+function alterarPopup10() {
+  document.getElementById("popup10").classList.toggle("ativa10");
+}
+function alterarPopup11() {
+  document.getElementById("popup11").classList.toggle("ativa11");
+}
+function alterarPopup12() {
+  document.getElementById("popup12").classList.toggle("ativa12");
+}
+function alterarPopup13() {
+  document.getElementById("popup13").classList.toggle("ativa13");
+}
+function alterarPopup14() {
+  document.getElementById("popup14").classList.toggle("ativa14");
+}
+function alterarPopup15() {
+  document.getElementById("popup15").classList.toggle("ativa15");
+}
+function alterarPopup16() {
+  document.getElementById("popup16").classList.toggle("ativa16");
+}
+function alterarPopup17() {
+  document.getElementById("popup17").classList.toggle("ativa17");
+}
+function alterarPopup18() {
+  document.getElementById("popup18").classList.toggle("ativa18");
+}
+function alterarPopup19() {
+  document.getElementById("popup19").classList.toggle("ativa19");
+}
+function alterarPopup20() {
+  document.getElementById("popup20").classList.toggle("ativa20");
+}
+function alterarPopup21() {
+  document.getElementById("popup21").classList.toggle("ativa21");
+}
+function alterarPopup22() {
+  document.getElementById("popup22").classList.toggle("ativa22");
+}
+function alterarPopup23() {
+  document.getElementById("popup23").classList.toggle("ativa23");
+}
+
